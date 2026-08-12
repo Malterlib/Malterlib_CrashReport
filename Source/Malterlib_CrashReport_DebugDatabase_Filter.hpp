7073b68c2473e218556a0ccb502019b01f1c0f16
Erik Olofsson <erik@olofsson.info>
2026-08-12 17:08:36 +0200

    Follow TCOptional's new move semantics parameter
    TCOptional gained an EOptionalMoveSemantics template parameter, so the
    filter overloads have to accept it to keep matching every optional.

--- Source/Malterlib_CrashReport_DebugDatabase_Filter.hpp
+++ Source/Malterlib_CrashReport_DebugDatabase_Filter.hpp
@@ -5,8 +5,8 @@
 
 namespace NMib::NCrashReport::NPrivate
 {
-	template <typename tf_CValue>
-	bool fg_IsExactFilter(NStorage::TCOptional<tf_CValue> const &_Value)
+	template <typename tf_CValue, NStorage::EOptionalMoveSemantics tf_MoveSemantics>
+	bool fg_IsExactFilter(NStorage::TCOptional<tf_CValue, tf_MoveSemantics> const &_Value)
 	{
 		if (!_Value)
 			return false;
